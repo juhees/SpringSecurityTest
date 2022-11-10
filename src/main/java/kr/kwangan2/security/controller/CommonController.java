e9abbd5f5bfac7bef19df16380aa7d96c29e7869
juhees <103228093+juhees@users.noreply.github.com>
2022-11-10 18:08:59 +0900

    created SpringSecurityTest project

---
+++ src/main/java/kr/kwangan2/security/controller/CommonController.java
@@ -0,0 +1,36 @@
+package kr.kwangan2.security.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth,Model model) {
+		log.info("auth:" + auth);
+		model.addAttribute("msg:","Access Denied");
+	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		log.info("error: "+error);
+		if(error != null)model.addAttribute("error", "Login Error Check Your Account");
+		if(logout != null)model.addAttribute("logout","Logout!!");
+		
+	}
+	
+	@GetMapping("/customLogout")
+	public void logoutGET() {
+		
+		log.info("custom logout");
+	}
+	
+	
+}
